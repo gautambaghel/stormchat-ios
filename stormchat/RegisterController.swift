67994c9aedfb910930e26ceeb9c8183add181e58
gautam <gautambaghel93@gmail.com>
2018-09-05 06:02:27 -0400

    minor view changes

--- stormchat/RegisterController.swift
+++ stormchat/RegisterController.swift
@@ -84,7 +84,7 @@ class RegisterController: UIViewController, UITextFieldDelegate ,UIPickerViewDat
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
             if self.view.frame.origin.y == 0{
-                self.view.frame.origin.y -= keyboardSize.height/2
+                self.view.frame.origin.y -= keyboardSize.height
             }
         }
     }
