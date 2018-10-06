6cadc6ddb9bde76344c139399bd1a1d2ddeb4445
gautam <gautambaghel93@gmail.com>
2018-10-05 23:21:46 -0400

    facebook button action added

--- stormchat/ViewController.swift
+++ stormchat/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import FBSDKLoginKit
 import GoogleSignIn
 
-class ViewController: UIViewController, UITextFieldDelegate , GIDSignInUIDelegate, GIDSignInDelegate {
+class ViewController: UIViewController, UITextFieldDelegate , GIDSignInUIDelegate, GIDSignInDelegate, FBSDKLoginButtonDelegate {
     
     @IBOutlet weak var email: UITextField!
     @IBOutlet weak var password: UITextField!
@@ -28,6 +28,9 @@ class ViewController: UIViewController, UITextFieldDelegate , GIDSignInUIDelegat
         
         // Google Sign in thing
         GIDSignIn.sharedInstance().uiDelegate = self
+        
+        // Facebook Sign in button delegate
+        self.fbLogin.delegate = self
         
         // Uncomment to automatically sign in the user.
         // GIDSignIn.sharedInstance().signInSilently()
@@ -73,6 +76,40 @@ class ViewController: UIViewController, UITextFieldDelegate , GIDSignInUIDelegat
         //if success display the email on label
         print(user.profile.email)
     }
+    
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        <#code#>
+    }
+    
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        <#code#>
+    }
 
+    @IBAction func loginAction(_ sender: UIButton) {
+        let url = URL(string: "https://stormchat.gautambaghel.com/api/v1/token")!
+        var request = URLRequest(url: url)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let email = self.email.text!
+        let password = self.password.text!
+        let postString = "email=\(String(describing: email))&pass=\(String(describing: password))"
+        print(postString)
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
+    }
 }
 
